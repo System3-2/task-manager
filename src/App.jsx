186ef84026806dde033a86e4dd46caef0b61c17c
OlojaMuyiwa <olojam4969@gmail.com>
2023-02-03 12:44:58 +0100

    finalised

--- src/App.jsx
+++ src/App.jsx
@@ -135,6 +135,12 @@ function App() {
                     id="tasks"
                     placeholder="Add Task"
                     className="m-6 px-4 py-2 outline-2 border-3 border-slate-900 bg-slate-200 rounded-sm" />
+                  <button type="submit"
+                    className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150 mx-6 md:mx-1"
+                  // onClick={() => setShowModal(false)}
+                  >
+                    {editing ? 'Edit' : 'Add Task'}
+                  </button>
                 </form>
                 {/*footer*/}
                 <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
@@ -145,12 +151,7 @@ function App() {
                   >
                     Close
                   </button>
-                  {/* <button type="button"
-                    className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                    onClick={() => setShowModal(false)}
-                  >
-                    Save Changes
-                  </button> */}
+
                 </div>
               </div>
             </div>
